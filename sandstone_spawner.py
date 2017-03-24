f8f35545d6bdc328779d474f6d1deae67f5dc0eb
Saurabh Sood <saurabhsood91@gmail.com>
2017-03-24 14:46:08 -0600

    add sandstone spawner

---
+++ sandstone_spawner.py
@@ -0,0 +1,55 @@
+from jupyterhub.spawner import LocalProcessSpawner
+from jupyterhub.utils import random_port
+
+from subprocess import Popen
+from tornado import gen
+import pipes
+import shutil
+import os
+
+START_SANDSTONE_SCRIPT = os.path.join(os.path.abspath(__file__), 'start_sandstone.sh')
+
+
+class SandstoneSpawner(LocalProcessSpawner):
+
+    @gen.coroutine
+    def start(self):
+        """Start the single-user server."""
+        self.port = random_port()
+        cmd = [START_SANDSTONE_SCRIPT]
+        env = self.get_env()
+
+        # cmd.extend(self.cmd)
+        # cmd.extend('{}'.format(self.port))
+        args = self.get_args()
+        # print(args)
+        cmd.extend(args)
+        # import os
+        # self.log.info(os.path.dirname(os.path.abspath(__file__)))
+
+        self.log.info("Spawning %s", ' '.join(pipes.quote(s) for s in cmd))
+        try:
+            self.proc = Popen(cmd, env=env,
+                preexec_fn=self.make_preexec_fn(self.user.name),
+                start_new_session=True, # don't forward signals
+            )
+        except PermissionError:
+            # use which to get abspath
+            script = shutil.which(cmd[0]) or cmd[0]
+            self.log.error("Permission denied trying to run %r. Does %s have access to this file?",
+                script, self.user.name,
+            )
+            raise
+
+        self.pid = self.proc.pid
+
+        if self.__class__ is not LocalProcessSpawner:
+            # subclasses may not pass through return value of super().start,
+            # relying on deprecated 0.6 way of setting ip, port,
+            # so keep a redundant copy here for now.
+            # A deprecation warning will be shown if the subclass
+            # does not return ip, port.
+            if self.ip:
+                self.user.server.ip = self.ip
+            self.user.server.port = self.port
+        return (self.ip or '127.0.0.1', self.port)
