18ac2998d3d88a0d1ab1bddc490a8e39df633a5e
Zebula Sampedro <sampedro@colorado.edu>
2017-04-04 12:51:28 -0600

    Restructured as installable python module.

---
+++ DESCRIPTION.rst
@@ -0,0 +1,4 @@
+Sandstone JupyterHub Spawner
+=======================
+
+Full documentation can be found on the GitHub page: https://github.com/SandstoneHPC/sandstone-spawner
--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
-import os
-import sys
+# -*- coding: utf-8 -*-
 from distutils.core import setup
 from setuptools import find_packages
 
-setup_args = dict(
-    name='sandstone_spawner',
+setup(
+    name='sandstone',
+    version='0.1.0',
+    author=u'Zebula Sampedro',
+    author_email='sampedro@colorado.edu',
     packages=find_packages(),
-    version="1.0",
-    description="""Spawner class to launch Sandstone HPC user instances""",
-    long_description="",
-    author="Zebula Sampedro",
-    author_email="sampedro@colorado.edu",
-    url="https://www.rc.colorado.edu/",
-    license="MIT",
-    platforms="Linux, Mac OS X",
-    keywords=['Interactive', 'Interpreter', 'Shell', 'Web'],
+    include_package_data=True,
+    url='https://github.com/SandstoneHPC/sandstone-spawner',
+    license='MIT, see LICENSE',
+    description=open('DESCRIPTION.rst').read(),
+    long_description='',
+    zip_safe=False,
+    install_requires=[
+        'jupyterhub==0.7.2',
+    ],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: Unix',
@@ -25,17 +27,5 @@ setup_args = dict(
         'Topic :: Text Editors :: Integrated Development Environments (IDE)',
         'Topic :: Terminals :: Terminal Emulators/X Terminals',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: JavaScript',
-    ],
-    install_requires=[
-        'jupyterhub==0.7.2',
     ]
 )
-
-
-def main():
-    setup(**setup_args)
-
-if __name__ == '__main__':
-    main()
