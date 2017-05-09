8b777be65822ac4c5c2f6c0009868384fbbac6d1
Zebula Sampedro <sampedro@colorado.edu>
2017-05-09 10:40:35 -0600

    Added manifest, bumped version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='sandstone-spawner',
-    version='0.1.0',
+    version='0.1.1',
     author=u'Zebula Sampedro',
     author_email='sampedro@colorado.edu',
     packages=find_packages(),
